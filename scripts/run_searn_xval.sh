6da8302a899835da79f64fef82432a6be282bca6
alonso <alonso@hum.ku.dk>
2015-01-06 19:09:39 +0100

    x

--- scripts/run_searn_xval.sh
+++ scripts/run_searn_xval.sh
@@ -19,19 +19,20 @@ do
     predout="$EVALPATH"$DESCRIPTOR.fold$fold.preds
     rawout="$EVALPATH"$DESCRIPTOR.fold$fold.raw
     bioout="$EVALPATH"$DESCRIPTOR.fold$fold.bio
+    bioconstout="$EVALPATH"$DESCRIPTOR.fold$fold.bioconstr
     evalout="$EVALPATH"$DESCRIPTOR.fold$fold.evalsimple
     evalcrout="$EVALPATH"$DESCRIPTOR.fold$fold.evalconstr
 
 
 
-    $VW -b $B -k -c -d $TRAINFILE --passes $passes --search_task sequence --search $nclasses -f $MODELNAME --keep b
+    $VW -b $B -k -c -d $TRAINFILE --passes $passes --search_task sequence --search $nclasses -f $MODELNAME
     $VW -t -d  $TESTFILE -i $MODELNAME -p $predout -r $rawout
 
     python "$HOME/"src/vwsearn2column.py $predout --class-map "$HOME"/data/res/da_map_bio_testtime.txt > $onecolout
 	python "$HOME/"src/da2conllinput.py $TESTFILE $onecolout --class-map $HOME/data/res/da_map_bio_testtime.txt > $bioout
 	perl "$HOME/"scripts/conlleval.pl -d "\t" < $bioout > $evalout
 
-	python "$HOME"/src/cr_contrains_on_raw_searn.py $TESTFILE $rawout --class-map "$HOME"/data/res/da_map_bio_testtime.txt  --supersense-dict $CONSTRAINDICT > $bioconstout
+	python "$HOME"/src/cr_contrains_on_raw_searn.py $TESTFILE $rawout --class-map "$HOME"/data/res/da_map_bio_testtime.txt --supersense-dict $CONSTRAINDICT > $bioconstout
 	perl "$HOME"/scripts/conlleval.pl -d "\t" < $bioconstout > $evalcrout
 
     rm $MODELNAME
