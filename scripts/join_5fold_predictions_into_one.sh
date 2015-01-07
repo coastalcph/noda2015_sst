a5905105cb4aafb95570ad8945927cb1df7fb6a8
alonso <alonso@hum.ku.dk>
2015-01-07 13:31:09 +0100

    joined 5foldpreds

---
+++ scripts/join_5fold_predictions_into_one.sh
@@ -0,0 +1,13 @@
+#nophr.fold0.evalsimple
+#alltags.fold0.evalconstr
+
+runspath=/home/alonso/proj/noda2015_sst/data/runs
+
+for evaltype in evalsimple evalconstr
+do
+    for sensetags in nophr alltags
+    do
+        outfile=$runspath/$sensetags.allfolds.$evaltype
+        cat $runspath/$sensetags.fold*.$evaltype > $outfile
+    done
+done
